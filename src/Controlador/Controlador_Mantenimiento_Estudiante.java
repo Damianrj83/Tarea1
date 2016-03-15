8547e4686c90004b784b1eae33578d00a78fb3cd
User <User@User-HP>
2016-03-15 10:40:18 -0600

    Proyecto primer commit

---
+++ src/Controlador/Controlador_Mantenimiento_Estudiante.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Vista.ManipulaEstudiantes;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author DELL
+ */
+public class Controlador_Mantenimiento_Estudiante implements ActionListener {
+
+   
+    
+    
+    private ManipulaEstudiantes manipulaEstudiantes;
+    
+    public Controlador_Mantenimiento_Estudiante(ManipulaEstudiantes manipulaEstudiantes) {
+        this.manipulaEstudiantes = manipulaEstudiantes;       
+    }
+
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+       if(ae.getActionCommand().equals("")){
+       
+       
+       }
+        if(ae.getActionCommand().equals("")){
+       
+       
+       }
+        
+          if(ae.getActionCommand().equals("")){
+       
+       
+       }
+          
+            if(ae.getActionCommand().equals("")){
+       
+       
+       }
+       
+        
+      
+    }//Fin ActionPerformed///
+    
+}
